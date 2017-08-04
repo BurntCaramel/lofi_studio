aba894266877ea3df502d89d4e94089f105cdd6d
Patrick Smith <patrick@burntcaramel.com>
2017-08-04 20:43:20 +1000

    Improve Screen pages

--- lib/lofi_play_web/views/screen_view.ex
+++ lib/lofi_play_web/views/screen_view.ex
@@ -1,8 +1,13 @@
 defmodule LofiPlayWeb.ScreenView do
   use LofiPlayWeb, :view
   alias LofiPlay.Preview.Bootstrap
+  alias Phoenix.HTML.Tag
   
   def preview_body(body) do
     Bootstrap.preview_text(body)
+  end
+
+  def container([do: block]) do
+    Tag.content_tag(:div, block, class: "container-fluid")
   end
 end
