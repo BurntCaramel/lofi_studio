3d6ab70a0cab7c06455e82174bbceeb72176892d
Patrick Smith <patrick@burntcaramel.com>
2017-08-22 01:03:21 +1000

    Add Twitter-inspired #post

--- lib/lofi_play/preview/ads.ex
+++ lib/lofi_play/preview/ads.ex
@@ -13,6 +13,10 @@ defmodule LofiPlay.Preview.Ads do
     "font-size: 14px",
   ] |> Enum.join("; ")
 
+  @post_body_style [
+    "font-size: 16px",
+  ] |> Enum.join("; ")
+
   @doc """
   Headline #ad #google
   """
@@ -26,7 +30,26 @@ defmodule LofiPlay.Preview.Ads do
     ] |> Enum.reject(&is_nil/1))
   end
 
+  @doc """
+  I am thoughtleadering like the boss of moss #post
+  """
+  def preview(%Lofi.Element{tags: %{"post" => {:flag, true}}}, %Lofi.Element{tags: tags, texts: texts}) do
+    body = Enum.join(texts)
+    name = get_content_tag(tags, "name")
+    username = get_content_tag(tags, "username")
+    Tag.content_tag(:div, [
+      Tag.content_tag(:div, [
+        (unless is_nil(name), do: Tag.content_tag(:strong, name, style: @post_body_style), else: nil),
+        " ",
+        (unless is_nil(username), do: Tag.content_tag(:span, "@#{username}", style: @post_body_style), else: nil),
+        " · ",
+        "16m"
+      ] |> Enum.reject(&is_nil/1)),
+      Tag.content_tag(:div, body, style: @post_body_style)
+    ])
+  end
+
   def preview(_element, _element) do
-    HTML.html_escape("")
+    nil
   end
 end
