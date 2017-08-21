05616c1f437bac65c03e9e4ed63c741be635c307
Patrick Smith <patrick@burntcaramel.com>
2017-08-22 00:20:12 +1000

    Improve Google Ads preview

--- lib/lofi_play/preview/ads.ex
+++ lib/lofi_play/preview/ads.ex
@@ -4,20 +4,25 @@ defmodule LofiPlay.Preview.Ads do
   alias Phoenix.HTML.Tag
 
   @headline_style [
-    "font-size: 18px",
+    "font-size: 16px",
     "font-weight: normal",
-    "color: #12c"
+    "color: #15c"
+  ] |> Enum.join("; ")
+
+  @description_style [
+    "font-size: 14px",
   ] |> Enum.join("; ")
 
   @doc """
-  Headline #ad #
+  Headline #ad #google
   """
+  # def preview([:ad, :google], %Lofi.Element{texts: texts, props: props}) do
   def preview(%Lofi.Element{tags: %{"ad" => {:flag, true}, "google" => {:flag, true}}}, %Lofi.Element{tags: tags, texts: texts}) do
     headline = Enum.join(texts)
     description = get_content_tag(tags, "description")
     Tag.content_tag(:div, [
       Tag.content_tag(:div, headline, class: "ad-headline", style: @headline_style),
-      (unless is_nil(description), do: Tag.content_tag(:p, description), else: nil)
+      (unless is_nil(description), do: Tag.content_tag(:p, description, style: @description_style), else: nil)
     ] |> Enum.reject(&is_nil/1))
   end
 
