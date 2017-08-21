e5b8c95796b04a09a0739545b4d6893592c09018
Patrick Smith <patrick@burntcaramel.com>
2017-08-21 17:29:20 +1000

    Display component type

--- lib/lofi_play_web/views/component_view.ex
+++ lib/lofi_play_web/views/component_view.ex
@@ -1,4 +1,13 @@
 defmodule LofiPlayWeb.ComponentView do
   use LofiPlayWeb, :view
   import LofiPlayWeb.LayoutHelpers
+
+  def display_component_type(type) do
+    case type do
+      1 -> "HTML"
+      2 -> "SVG"
+      3 -> "Primitives"
+      _ -> "Unknown"
+    end
+  end
 end
