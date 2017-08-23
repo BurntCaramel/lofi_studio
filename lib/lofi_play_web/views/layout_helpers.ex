d60c73bc8baf0f4c385a7e7cbc998be2b39ae45f
Patrick Smith <patrick@burntcaramel.com>
2017-08-23 11:24:40 +1000

    Add margin options to container()

--- lib/lofi_play_web/views/layout_helpers.ex
+++ lib/lofi_play_web/views/layout_helpers.ex
@@ -13,4 +13,21 @@ defmodule LofiPlayWeb.LayoutHelpers do
   def container([do: block]) do
     Tag.content_tag(:div, block, class: "container-fluid")
   end
+
+  @doc """
+  Make a div.container-fluid
+  """
+  def container(opts, [do: block]) do
+    # Margins
+    mt = Keyword.get(opts, :mt, 0)
+    mb = Keyword.get(opts, :mb, 0)
+
+    class = [
+      "container-fluid",
+      "mt-#{mt}",
+      "mb-#{mb}"
+    ] |> Enum.join(" ")
+
+    Tag.content_tag(:div, block, class: class)
+  end
 end
