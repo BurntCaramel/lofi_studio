11026e963a816d173a012004a6ad26a84394305a
Patrick Smith <patrick@burntcaramel.com>
2017-08-25 18:58:20 +1000

    Add ingredients form to screen #show

--- lib/lofi_play_web/views/screen_view.ex
+++ lib/lofi_play_web/views/screen_view.ex
@@ -1,5 +1,6 @@
 defmodule LofiPlayWeb.ScreenView do
   use LofiPlayWeb, :view
+  alias LofiPlayWeb.IngredientsHelpers
   alias LofiPlay.Preview.Bootstrap
   
   def preview_body(body, components: components) do
@@ -8,5 +9,9 @@ defmodule LofiPlayWeb.ScreenView do
 
   def preview_in_iframe(conn, screen, layout) do
     content_tag(:iframe, '', class: "screen-preview-iframe", src: screen_preview_path(conn, :show, screen, layout: layout))
+  end
+
+  def ingredients_preview_form(conn, screen) do
+    IngredientsHelpers.ingredients_preview_form(conn, screen.ingredients, [type: "screen", item_id: screen.id])
   end
 end
