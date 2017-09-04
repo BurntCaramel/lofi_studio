fd78240d308681b93243bd7c4f4cdc3f53dc0db7
Patrick Smith <patrick@burntcaramel.com>
2017-09-04 08:43:42 -0700

    Use handlebars-style {{…}} for punching variables into components

--- lib/lofi_play/preview/components.ex
+++ lib/lofi_play/preview/components.ex
@@ -2,6 +2,12 @@ defmodule LofiPlay.Preview.Components do
   alias LofiPlay.Preview
   alias Phoenix.HTML
 
+  defp replace_key(body, key, replacement) do
+    regex = "{{[\s]*#{Regex.escape(key)}[\s]*}}" |> Regex.compile!
+    Regex.replace(regex, body, replacement)
+    # String.replace(body, "@#{key}", replacement)
+  end
+
   def render_html_component(body, ingredients, %Lofi.Element{tags: tags, texts: texts}) do
     ingredient_infos = Preview.Lofi.parse_ingredients(ingredients)
 
@@ -16,7 +22,7 @@ defmodule LofiPlay.Preview.Components do
         _ ->
           Preview.Lofi.get_content_tag(tags, key, default)
       end
-      String.replace(html, "@#{key}", replacement)
+      replace_key(html, key, replacement)
     end)
 
     HTML.raw(adjusted)
@@ -32,7 +38,7 @@ defmodule LofiPlay.Preview.Components do
       if is_nil(replacement) do
         html
       else
-        String.replace(html, "@#{key}", replacement)
+        replace_key(html, key, replacement)
       end
     end)
 
@@ -54,7 +60,7 @@ defmodule LofiPlay.Preview.Components do
       if is_nil(replacement) do
         html
       else
-        String.replace(html, "@#{key}", replacement)
+        replace_key(html, key, replacement)
       end
     end)
 
