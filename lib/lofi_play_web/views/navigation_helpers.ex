e04bb97db2fa0866dfec6308304f559349aa6de1
Patrick Smith <patrick@burntcaramel.com>
2017-10-23 23:12:50 +1100

    Improve navbar

--- lib/lofi_play_web/views/navigation_helpers.ex
+++ lib/lofi_play_web/views/navigation_helpers.ex
@@ -1,19 +1,29 @@
 defmodule LofiPlayWeb.NavigationHelpers do
-  
-    @moduledoc """
-    Conveniences for navigation & search.
-    """
+  @moduledoc """
+  Conveniences for navigation & search.
+  """
 
-    use Phoenix.HTML
-    import LofiPlayWeb.Router.Helpers
-    alias LofiPlay.Content.Search
+  use Phoenix.HTML
+  import LofiPlayWeb.Router.Helpers
+  alias LofiPlay.Content.Search
 
-    
-    def search_form(conn) do
-      changeset = Map.get(conn.assigns, :search_changeset, Search.changeset(%Search{}, %{}))
   
-      form_for changeset, search_path(conn, :index), [method: "get"], fn f ->
-        text_input(f, :query, placeholder: "Search Collected", aria: [label: "Search Collected"], class: "form-control")
-      end
+  def search_form(conn) do
+    changeset = Map.get(conn.assigns, :search_changeset, Search.changeset(%Search{}, %{}))
+
+    form_for changeset, search_path(conn, :index), [method: "get"], fn f ->
+      text_input(f, :query, placeholder: "Search", aria: [label: "Search Collected"], class: "form-control")
     end
+  end
+
+  def nav_link(text, opts) do
+    link_class = case Keyword.get(opts, :class) do
+      nil -> "nav-link"
+      class -> "#{class} nav-link"
+    end
+
+    content_tag(:li, [
+      link(text, Keyword.put(opts, :class, link_class))
+    ], class: "nav-item")
+  end
 end
