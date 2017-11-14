a45f48ed53181be873f525621e189575a420dcd3
Patrick Smith <patrick@burntcaramel.com>
2017-11-14 23:30:00 +1100

    Fix rendering of invalid ingredients

--- lib/lofi_play/preview/components.ex
+++ lib/lofi_play/preview/components.ex
@@ -8,26 +8,80 @@ defmodule LofiPlay.Preview.Components do
     # String.replace(body, "@#{key}", replacement)
   end
 
-  def render_html_component(body, ingredients, %Lofi.Element{tags: tags, texts: texts}) do
-    ingredient_infos = Preview.Lofi.parse_ingredients(ingredients)
+  defp replace_html_for_ingredient_info(ingredient_info, html, %Lofi.Element{tags: tags, texts: texts}) do
+    case ingredient_info do
+      {:ok, {key, {_type, default, _choices}}} ->
+        replacement = case key do
+          # @texts uses texts of element
+          # TODO: rename to @content
+          "texts" ->
+            Enum.join(texts)
+          "content" ->
+            Enum.join(texts)
+          # Otherwise look up tag
+          _ ->
+            Preview.Lofi.get_content_tag(tags, key, default)
+        end
+        replace_key(html, key, replacement)
+      {:error, _} ->
+        html
+    end
+  end
 
-    adjusted = ingredient_infos
-    |> Enum.reduce(body, fn({key, {_type, default, _choices}}, html) ->
-      replacement = case key do
-        # @texts uses texts of element
-        # TODO: rename to @content
-        "texts" ->
-          Enum.join(texts)
-        "content" ->
-          Enum.join(texts)
-        # Otherwise look up tag
-        _ ->
-          Preview.Lofi.get_content_tag(tags, key, default)
-      end
-      replace_key(html, key, replacement)
-    end)
+  defp replace_html_for_ingredient_info(ingredient_info, html, values) do
+    case ingredient_info do
+      {:ok, {key, {_type, default, choices}}} ->
+        replacement = Map.get(values, key)
+        
+        if is_nil(replacement) do
+          html
+        else
+          replace_key(html, key, replacement)
+        end
+      {:error, _} ->
+        html
+    end
+  end
 
-    HTML.raw(adjusted)
+  defp replace_html_for_ingredient_info(ingredient_info, html) do
+    case ingredient_info do
+      {:ok, {key, {_type, default, choices}}} ->
+        replacement = if Enum.empty?(choices) do
+          default
+        else
+          Enum.random(choices).texts
+          |> Enum.join
+        end
+  
+        if is_nil(replacement) do
+          html
+        else
+          replace_key(html, key, replacement)
+        end
+      {:error, _} ->
+        html
+    end
+  end
+
+  def render_html_component(body, ingredients, lofi_element) do
+    ingredients
+    |> Preview.Lofi.parse_ingredients
+    |> Enum.reduce(body, &replace_html_for_ingredient_info(&1, &2, lofi_element))
+    |> HTML.raw
+  end
+
+  def render_html_component_preview(body, ingredients, values) do
+    ingredients
+    |> Preview.Lofi.parse_ingredients
+    |> Enum.reduce(body, &replace_html_for_ingredient_info(&1, &2, values))
+    |> HTML.raw
+  end
+
+  def render_html_component_preview(body, ingredients) do
+    ingredients
+    |> Preview.Lofi.parse_ingredients
+    |> Enum.reduce(body, &replace_html_for_ingredient_info/2)
+    |> HTML.raw
   end
 
   def render_lofi_component(body, ingredients_s, values) do
@@ -40,44 +94,5 @@ defmodule LofiPlay.Preview.Components do
     # ingredient_infos = Preview.Lofi.parse_ingredients(ingredients_s)
 
     Preview.Bootstrap.preview_text(body, ingredients_s)
-  end
-
-  def render_html_component_preview(body, ingredients, values) do
-    ingredient_infos = Preview.Lofi.parse_ingredients(ingredients)
-
-    adjusted = ingredient_infos
-    |> Enum.reduce(body, fn({key, {_type, default, choices}}, html) ->
-      replacement = Map.get(values, key)
-
-      if is_nil(replacement) do
-        html
-      else
-        replace_key(html, key, replacement)
-      end
-    end)
-
-    HTML.raw(adjusted)
-  end
-
-  def render_html_component_preview(body, ingredients) do
-    ingredient_infos = Preview.Lofi.parse_ingredients(ingredients)
-
-    adjusted = ingredient_infos
-    |> Enum.reduce(body, fn({key, {_type, default, choices}}, html) ->
-      replacement = if Enum.empty?(choices) do
-        default
-      else
-        Enum.random(choices).texts
-        |> Enum.join
-      end
-
-      if is_nil(replacement) do
-        html
-      else
-        replace_key(html, key, replacement)
-      end
-    end)
-
-    HTML.raw(adjusted)
   end
 end
