fe4420d5230fd446399c81555f90a3bc8d14ee05
Patrick Smith <patrick@burntcaramel.com>
2017-11-14 23:45:49 +1100

    Seed bootstrap alert

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+%LofiPlay.Content.Component{
+  name: "Alert",
+  tags: "#alert",
+  type: 2,
+  body: """
+<div class="alert alert-{{variation}}">{{ content }}</div>
+""",
+  ingredients: """
+@content: #text
+- Message
+@variation: #choice
+- info
+- success
+- danger
+"""
+}
+|> LofiPlay.Repo.insert!
